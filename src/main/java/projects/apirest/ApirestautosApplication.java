6ed6a2b7441bf82bba265ba360c241071efa3222
FranKR02 <frankdiego007@hotmail.com>
2023-11-17 14:01:22 -0500

    Configuración inicial

---
+++ src/main/java/projects/apirest/ApirestautosApplication.java
@@ -0,0 +1,13 @@
+package projects.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestautosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestautosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projects/apirest/ApirestautosApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestautosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
